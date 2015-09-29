35b61de46a609e15e9f5dbae9a7f34ed6573a9e6
ptwmobility <ptwmobility@PTWAs-MacBook-Pro-3.local>
2015-09-29 10:56:34 -0700

    static analyzer kickoff
    static analyzer kickoff demo

--- iOS/ExperiBank/LoginViewController.m
+++ iOS/ExperiBank/LoginViewController.m
@@ -42,7 +42,7 @@
 - (void)viewDidAppear:(BOOL)animated {
     
     
-    [super viewDidAppear:animated];
+    //[super viewDidAppear:animated];
     usernameTextField.text =@"";
     passwordTextField.text =@"";
 }
